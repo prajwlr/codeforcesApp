45f813f6e3ca7975331aae40bdd4ba5ca8bc9cc1
Mafooz <kmahfooz277@gmail.com>
2020-07-27 15:59:03 +0530

    Integrated contest list beta

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements ContestListAdapter.ItemClicked {
     private static final String location = "contestList.json";
@@ -33,58 +32,19 @@ public class MainActivity extends AppCompatActivity implements ContestListAdapte
     RecyclerView.LayoutManager layoutManager;
     ArrayList<ContestCard> contestsData;
     SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.list);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        contestsData = new ArrayList<ContestCard>();
-        int[] arr={0};
-        JSONObject contestListWrapper = new JSONObject();
-        JSONArray contestList = new JSONArray();
-        contestListWrapper.put("status", "false");
-        contestListWrapper.put("result", contestList);
-        GetOngoingContest getContestData = new GetOngoingContest(contestListWrapper);
-        getContestData.start();
-        try {
-            getContestData.join();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        String status = (String)contestListWrapper.get("status");
-        if(status.equals("false")){
-//            contestsData.add(new ContestCard((long) 1, false, "contestName"));
-            contestList = readFile();
-        }
-        else{
-            writeFile(contestList);
-        }
-        for(int i=0;i<contestList.size();i++) {
-            contestsData.add(new ContestCard((Long) ((JSONObject)contestList.get(i)).get("divId"), false, (String) ((JSONObject)contestList.get(i)).get("contestName")));
-        }
-        myAdapter = new ContestListAdapter(this, contestsData);
-        myAdapter = new ContestListAdapter(this, contestsData);
-        recyclerView.setAdapter(myAdapter);
-
+//        setRecyclerView();
+//        setRecyclerView();
         swipeRefreshLayout = findViewById(R.id.swipe);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeRefreshLayout.setRefreshing(false);
-                //your code on swipe refresh
-                //we are checking networking connectivity
-                boolean connection=isNetworkAvailable();
-                if(connection){
-                    Toast.makeText(getApplicationContext(),"Internet is on",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Internet is off",Toast.LENGTH_SHORT).show();
-                }
-
+                setRecyclerView();
             }
         });
     }
@@ -94,25 +54,24 @@ public class MainActivity extends AppCompatActivity implements ContestListAdapte
 
         Toast.makeText(this, "Status: " + contestsData.get(index).isRegistered(), Toast.LENGTH_SHORT).show();
     }
-    public JSONArray readFile(){
+
+    public JSONArray readFile() {
         FileInputStream fis = null;
-        try{
+        try {
             fis = openFileInput(location);
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader br = new BufferedReader(isr);
             StringBuilder sb = new StringBuilder();
             String content;
-            while((content=br.readLine())!=null){
+            while ((content = br.readLine()) != null) {
                 sb.append(content).append("\n");
             }
             JSONParser parser = new JSONParser();
             return (JSONArray) parser.parse(sb.toString());
-        }
-        catch (IOException | ParseException e){
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(fis!=null) {
+        } finally {
+            if (fis != null) {
                 try {
                     fis.close();
                 } catch (IOException e) {
@@ -122,18 +81,17 @@ public class MainActivity extends AppCompatActivity implements ContestListAdapte
         }
         return new JSONArray();
     }
+
     public void writeFile(JSONArray jsonArray) {
         String content = jsonArray.toJSONString();
         FileOutputStream fos = null;
         try {
             fos = openFileOutput(location, MODE_PRIVATE);
             fos.write(content.getBytes());
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(fos!=null) {
+        } finally {
+            if (fos != null) {
                 try {
                     fos.close();
                 } catch (IOException e) {
@@ -143,9 +101,38 @@ public class MainActivity extends AppCompatActivity implements ContestListAdapte
         }
     }
 
+    public void setRecyclerView() {
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        contestsData = new ArrayList<ContestCard>();
+        JSONObject contestListWrapper = new JSONObject();
+        JSONArray contestList = new JSONArray();
+        contestListWrapper.put("status", "false");
+        contestListWrapper.put("result", contestList);
+        GetOngoingContest getContestData = new GetOngoingContest(contestListWrapper);
+        getContestData.start();
+        try {
+            getContestData.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String status = (String) contestListWrapper.get("status");
+        if (status.equals("false")) {
+            contestList = readFile();
+        } else {
+            writeFile(contestList);
+        }
+        for (int i = 0; i < contestList.size(); i++) {
+            contestsData.add(new ContestCard((Long) ((JSONObject) contestList.get(i)).get("divId"), false, (String) ((JSONObject) contestList.get(i)).get("contestName")));
+        }
+        myAdapter = new ContestListAdapter(this, contestsData);
+        myAdapter = new ContestListAdapter(this, contestsData);
+        recyclerView.setAdapter(myAdapter);
+    }
 
     public boolean isNetworkAvailable(){
-
         ConnectivityManager connectivityManager=(ConnectivityManager) this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
         return networkInfo !=null;
