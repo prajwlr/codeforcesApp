ea291356f98db577345c135aa355b01c18039990
akarsh999 <65600816+akarsh999@users.noreply.github.com>
2020-08-03 14:21:26 +0530

    added Info personal to navbar fixed back button fixed toggle button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -69,16 +70,35 @@ public class MainActivity extends AppCompatActivity implements ContestListAdapte
                     startActivity(intent);
                     return true;
                 }
+                if(id==R.id.nav_profile){
+                    Intent intent = new Intent(MainActivity.this, InfoPersonal.class);
+                    startActivity(intent);
+                    return true;
+                }
                 return true;
             }
         });
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onItemClicked(int index) {
 
         Toast.makeText(this, "Status: " + contestsData.get(index).isRegistered(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(dl.isDrawerOpen(GravityCompat.START)){
+            dl.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 
     public JSONArray readFile() {
